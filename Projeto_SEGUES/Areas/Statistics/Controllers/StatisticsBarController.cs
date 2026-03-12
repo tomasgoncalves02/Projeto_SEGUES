263bdaee6c65f990a46b0018dad4a53e57ff0d7d
Diogo Pinheiro <202100729@estudantes.ips.pt>
2026-03-12 17:30:05 +0000

    Criação das pastas para a parte da estatistica

---
+++ Projeto_SEGUES/Areas/Statistics/Controllers/StatisticsBarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_SEGUES.Areas.Statistics.Controllers
+{
+    public class StatisticsBarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
