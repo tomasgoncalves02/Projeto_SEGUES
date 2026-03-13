738cc126a85a8a26eafc043015cac667f59db3ab
Diogo Pinheiro <202100729@estudantes.ips.pt>
2026-03-13 23:34:57 +0000

    Inicio das estatisticas bar

--- Projeto_SEGUES/Areas/Statistics/Controllers/StatisticsBarController.cs
+++ Projeto_SEGUES/Areas/Statistics/Controllers/StatisticsBarController.cs
@@ -1,13 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Projeto_SEGUES.Services;
 
 namespace Projeto_SEGUES.Areas.Statistics.Controllers
 {
     [Area("Statistics")]
     public class StatisticsBarController : Controller
     {
+        private readonly IStatisticsService _statisticsService;
+
+
+        public StatisticsBarController(IStatisticsService statisticsService)
+        {
+            _statisticsService = statisticsService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBarStats(string period = "Dia")
+        {
+            var result = await _statisticsService.GetTicketsStats(period);
+            return Json(result);
+        }
+
     }
 }
