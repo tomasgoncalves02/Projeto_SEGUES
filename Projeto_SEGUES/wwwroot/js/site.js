842b4d83129b166622569d4aef5c2d204ca16c60
Tomas Goncalves <tomasgoncalves02@gmail.com>
2026-03-14 03:26:11 +0000

    Add popup ementa bar e refeitorio e add card ementa bar

--- Projeto_SEGUES/wwwroot/js/site.js
+++ Projeto_SEGUES/wwwroot/js/site.js
@@ -167,7 +167,26 @@ function showSwal(options) {
     return Swal.fire(config); // Return the promise
 }
 
-
+function escolherEmenta() {
+    Swal.fire({
+        title: 'Qual ementa deseja visualizar?',
+        icon: 'question',
+        showCancelButton: true,
+        showDenyButton: true,
+        confirmButtonText: 'Refeitório',
+        denyButtonText: 'Bar',
+        cancelButtonText: 'Cancelar',
+        confirmButtonColor: '#009697', 
+        denyButtonColor: '#009697',    
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.open("https://software.movelife.net/pt-PT/Menus/PublicCC/Tj6o3O_vCFB2LmCmm9VUjw%3d%3d", "_blank");
+        } else if (result.isDenied) {
+            window.open(" https://software.movelife.net/pt-PT/Menus/PublicCC/Tj6o3O_vCFDXvHU0nbgTmg%3d%3d?DaySelected=13%2F03%2F2026&capit=5&idzone=1616", "_blank");
+        }
+    });
+}
 
 /*
  * Password Visibility Toggle
