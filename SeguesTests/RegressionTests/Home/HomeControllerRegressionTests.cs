8a0929d9f1d8c6d8faf810d4c4f5ccab896c0e66
PedroDiasdaSilvaJesus <202100741@estudantes.ips.pt>
2026-03-28 17:26:05 +0000

    New tests regression, unit, security and integration tests for Home Controllers and added new regression tests for users

---
+++ SeguesTests/RegressionTests/Home/HomeControllerRegressionTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Projeto_SEGUES.Models.Enums;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SeguesTests.RegressionTests.Home
+{
+    public class HomeControllerRegressionTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+
+        public HomeControllerRegressionTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _client = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Error_Default_ReturnsSuccessStatusCodeAndHtml()
+        {
+            var response = await _client.GetAsync("/Home/Error");
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+        }
+
+        [Fact]
+        public async Task Error_WithSpecificCode_ReturnsSuccessStatusCodeAndHtml()
+        {
+            var errorCode = (int)AppErrors.UserNotFound;
+            var response = await _client.GetAsync($"/Home/Error?errorCode={errorCode}");
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+        }
+    }
+}
---
+++ SeguesTests/RegressionTests/Users/UserControllerRegressionTests.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Projeto_SEGUES.Data;
+using Projeto_SEGUES.Models.Enums;
+using Projeto_SEGUES.Models.User;
+using SeguesTests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SeguesTests.RegressionTests.Users
+{
+    public class UserControllerRegressionTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public UserControllerRegressionTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Index_UserNotFoundInDatabase_ReturnsChallengeRedirect()
+        {
+            var client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+                    if (descriptor != null) services.Remove(descriptor);
+
+                    services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("UserRegDb1"));
+
+                    services.AddAuthentication("Test")
+                        .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("Test", null);
+                });
+            }).CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = false });
+
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+            }
+
+            var response = await client.GetAsync("/User/User/Index");
+
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task UpdateProfile_InvalidModelState_ReturnsViewWithErrors()
+        {
+            var client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+                    if (descriptor != null) services.Remove(descriptor);
+
+                    services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("UserRegDb2"));
+
+                    services.AddAuthentication("Test")
+                        .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("Test", null);
+
+                    services.Configure<MvcOptions>(options => {
+                        options.Filters.Add(new IgnoreAntiforgeryTokenAttribute());
+                    });
+                });
+            }).CreateClient();
+
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                var cat = new UserCategory { Name = "Cliente" };
+                db.UserCategory.Add(cat);
+
+                var role = new Role { Name = "Client", NormalizedName = "CLIENT", DisplayName ="Cliente" };
+                db.Roles.Add(role);
+
+                var user = new Student
+                {
+                    Id = "pedro-77",
+                    UserName = "Pedro",
+                    Email = "pedro@segues.pt",
+                    FirstName = "Pedro",
+                    LastName = "Original",
+                    BirthDate = new DateTime(2000, 1, 1),
+                    Gender = Gender.Male,
+                    UserCategory = cat,
+                    StudentNumber = "2024001"
+                };
+
+                db.Users.Add(user);
+                await db.SaveChangesAsync();
+            }
+
+            var content = new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                { "Id", "pedro-77" },
+                { "FirstName", "" }
+            });
+
+            var response = await client.PostAsync("/User/User/UpdateProfile", content);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
